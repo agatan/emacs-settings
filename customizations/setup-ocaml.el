ce2342139874f938747d1b476e923700975c29a6
agatan <agatan039@gmail.com>
2015-10-19 12:46:17 +0900

    Modify OCaml settings

--- customizations/setup-ocaml.el
+++ customizations/setup-ocaml.el
@@ -3,16 +3,18 @@
 (use-package tuareg
   :mode (("\\.ml" . tuareg-mode))
   :init
-  (add-hook 'tuareg-mode-hook
-            '(lambda ()
-               (local-set-key "\C-c;" 'ocamlspot-query)
-               (local-set-key "\C-c:" 'ocamlspot-query-interface)
-               (local-set-key "\C-c'" 'ocamlspot-query-uses)
-               (local-set-key "\C-c\C-t" 'ocamlspot-type)
-               (local-set-key "\C-c\C-i" 'ocamlspot-xtype)
-               (local-set-key "\C-c\C-y" 'ocamlspot-type-and-copy)
-               (local-set-key "\C-ct" 'caml-types-show-type)
-               (local-set-key "\C-cp" 'ocamlspot-pop-jump-stack)))
+  (progn
+    (add-hook 'tuareg-mode-hook
+              '(lambda ()
+                 (local-set-key "\C-c;" 'ocamlspot-query)
+                 (local-set-key "\C-c:" 'ocamlspot-query-interface)
+                 (local-set-key "\C-c'" 'ocamlspot-query-uses)
+;                 (local-set-key "\C-c\C-t" 'ocamlspot-type)
+                 (local-set-key "\C-c\C-i" 'ocamlspot-xtype)
+                 (local-set-key "\C-c\C-y" 'ocamlspot-type-and-copy)
+                 (local-set-key "\C-ct" 'caml-types-show-type)
+                 (local-set-key "\C-cp" 'ocamlspot-pop-jump-stack)))
+    (add-hook 'tuareg-mode-hook 'merlin-mode))
   :config
   (setq opam-share
         (substring
@@ -21,4 +23,5 @@
   (add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
   (require 'utop)
   (require 'ocp-indent)
-  (require 'ocamlspot))
+  (require 'ocamlspot)
+  (require 'merlin))
